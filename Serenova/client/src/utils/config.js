f1416852c449690f6f6c2a23bf16eb05b9ec27da
Aryan030204 <artistaryan18@gmail.com>
2025-08-01 10:09:01 +0530

    update SERVER_URL to point to production server

--- Serenova/client/src/utils/config.js
+++ Serenova/client/src/utils/config.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = "http://localhost:3000/api";
+export const SERVER_URL = "https://serenova-server.onrender.com/api";
 export const PYTHON_APP_URL = "http://127.0.0.1:5000";
 export const EMAIL_TEMPLATE_ID = "template_aht6na8";
 export const EMAIL_SERVICE_ID = "service_1o4ehaj";
