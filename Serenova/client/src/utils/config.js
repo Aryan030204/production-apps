b5da2adea8cd81fb7f6022d653f62b5f961386b9
Aryan030204 <artistaryan18@gmail.com>
2025-08-01 09:59:08 +0530

    updated

---
+++ Serenova/client/src/utils/config.js
@@ -0,0 +1,8 @@
+export const SERVER_URL = "http://localhost:3000/api";
+export const PYTHON_APP_URL = "http://127.0.0.1:5000";
+export const EMAIL_TEMPLATE_ID = "template_aht6na8";
+export const EMAIL_SERVICE_ID = "service_1o4ehaj";
+export const EMAIL_PUBLIC_KEY = "NcCPchw9egU4lcpDp";
+export const OPENWEATHER_API_KEY = "b3ea7b6499230e0cc50fc1062319d21f";
+export const WHATSAPP_ALERT_MESSAGE="I am in danger and need immediate help. Please contact the authorities or someone nearby who can assist me. This message was sent through the SOS feature of my safety app. Please treat this as an urgent emergency.I am relying on you. Please act fast."
+export const PRODUCTION_URL="https://serenova-server-node.onrender.com/api"
