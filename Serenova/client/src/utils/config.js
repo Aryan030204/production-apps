76f15c45ac35bbbcc58ad89f356bd3d595c0510a
Aryan030204 <artistaryan18@gmail.com>
2025-11-04 11:53:32 +0530

    updated openweather api key

--- Serenova/client/src/utils/config.js
+++ Serenova/client/src/utils/config.js
@@ -3,6 +3,6 @@ export const PYTHON_APP_URL = "https://serenova-flask-api.onrender.com";
 export const EMAIL_TEMPLATE_ID = "template_aht6na8";
 export const EMAIL_SERVICE_ID = "service_1o4ehaj";
 export const EMAIL_PUBLIC_KEY = "NcCPchw9egU4lcpDp";
-export const OPENWEATHER_API_KEY = "39a150e7b0530b8bb6371b204299aca7";
+export const OPENWEATHER_API_KEY = "b3ea7b6499230e0cc50fc1062319d21f";
 export const WHATSAPP_ALERT_MESSAGE="I am in danger and need immediate help. Please contact the authorities or someone nearby who can assist me. This message was sent through the SOS feature of my safety app. Please treat this as an urgent emergency.I am relying on you. Please act fast."
 export const PRODUCTION_URL="https://serenova-eta.vercel.app/"
